e144fc0f4ff4d4e2b8439758b1117e026729a734
lailagebhard <lailagebhard@gmail.com>
2022-12-01 16:49:04 -0300

    personagens Laila

---
+++ Rpg-TextBased-Devs2blu/src/Herois/Eladrin.java
@@ -0,0 +1,41 @@
+package Herois;
+import FantasyOne.Atributos;
+
+public class Eladrin extends Atributos {
+	
+	public Eladrin(String name, int maxHp, int xp, int hp, int pocao, int nivel, int mp) {
+		super(name, maxHp, xp, hp, pocao, nivel, mp);
+	}	
+	
+	@Override
+	public int ataqueBasico() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueBasico2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defesa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+}
---
+++ Rpg-TextBased-Devs2blu/src/Inimigos/VilaoDuergar.java
@@ -0,0 +1,41 @@
+package Inimigos;
+import FantasyOne.Atributos;
+
+public class VilaoDuergar extends Atributos{
+	
+	public VilaoDuergar(String name, int maxHp, int xp, int hp, int pocao, int nivel, int mp) {
+		super(name, maxHp, xp, hp, pocao, nivel, mp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int ataqueBasico() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueBasico2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defesa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
