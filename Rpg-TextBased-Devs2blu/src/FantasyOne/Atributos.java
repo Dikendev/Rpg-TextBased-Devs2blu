aad392748306378ea441c69ae7171a89190a0db2
Caio Neves <nevescaioneves@gmail.com>
2022-12-01 15:54:22 -0300

    estrutura parcial pronta

---
+++ Rpg-TextBased-Devs2blu/src/FantasyOne/Atributos.java
@@ -0,0 +1,25 @@
+package FantasyOne;
+
+public abstract class Atributos {
+	//variaveis //para todos os personagens
+	
+	public String name;
+	public int maxHp, hp, xp, pocao, nivel, mp;
+
+	public Atributos(String name, int maxHp, int xp, int hp, int pocao, int nivel, int mp) {
+		this.name = name;	
+		this.maxHp = maxHp;
+		this.xp = xp;
+		this.hp = hp;
+		this.pocao = pocao;
+		this.nivel = nivel;
+		this.mp = mp;
+	}
+	
+	//metodos
+	public abstract int ataqueBasico();
+	public abstract int ataqueBasico2();
+	public abstract int ataqueEspecial();
+	public abstract int ataqueEspecial2();
+	public abstract int defesa();
+}
