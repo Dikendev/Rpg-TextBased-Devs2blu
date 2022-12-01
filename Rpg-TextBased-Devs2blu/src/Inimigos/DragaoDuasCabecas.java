676fa59dd08e2c712299d5c31e8889c2e50107f9
Helena Correa Ribeiro <helena.correa84@gmail.com>
2022-12-01 16:13:00 -0300

    Criação Personagens Helena

---
+++ Rpg-TextBased-Devs2blu/src/Inimigos/DragaoDuasCabecas.java
@@ -0,0 +1,42 @@
+package Inimigos;
+
+import FantasyOne.Atributos;
+
+public class DragaoDuasCabecas extends Atributos {
+
+	public DragaoDuasCabecas(String name, int maxHp, int xp, int hp, int pocao, int nivel, int mp) {
+		super(name, maxHp, xp, hp, pocao, nivel, mp);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int ataqueBasico() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueBasico2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int ataqueEspecial2() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int defesa() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
